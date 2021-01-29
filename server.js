40f17ee8a5e24abcfd47ae402cff32e1191751ce
Sanjeev Ray <sanjeevat1996@gmail.com>
2021-01-29 19:19:37 +0530

    added the server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({path: './config.env'});
+
+const app = require('./app');
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port: ${process.env.PORT}`)
+}); 
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log('Database connection successfull!'))
+
